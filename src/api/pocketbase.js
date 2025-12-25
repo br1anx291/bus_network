7211990e86fc083b4db18458cc5b47b135756c9f
fructose1203 <httmy2003a2@gmail.com>
2025-12-25 19:17:15 +0700

    update 25_12

--- src/api/pocketbase.js
+++ src/api/pocketbase.js
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 
 // Thay bằng URL thật bạn nhận được
-const url = 'https://pocketbase.lukaxzs.myaddr.io/'; 
+const url = import.meta.env.VITE_POCKETBASE_URL;
 const pb = new PocketBase(url);
 
 // Tắt chế độ tự hủy request để tránh lỗi khi gọi nhiều API cùng lúc
